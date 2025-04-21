52c4e60d6dfa3fc5820e2440bf1c37b500dd2bae
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-21 06:49:40 -0300

    feat(eslint): add Prettier integration and update ESLint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <h1>Hello World</h1>
     </ThemeProvider>
-  )
+  );
 }
-
